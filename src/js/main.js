20f237601d30c9cc1eda1d3d22670e7c99620c97
Eh3uN <e.naghibi@yahoo.com>
2026-01-06 20:02:07 +0330

    instagram link and bugs fixed

--- src/js/main.js
+++ src/js/main.js
@@ -10,5 +10,38 @@ const telegramLink = document.getElementById("telegram-link");
   vpnConfirm.addEventListener("click", function() {
     vpnModal.classList.add("hidden"); // مخفی کردن modal
     // باز کردن تلگرام در تب جدید
-    window.open("https://t.me/USERNAME", "_blank", "noopener,noreferrer");
+    window.open("https://t.me/", "_blank", "noopener,noreferrer");
   });
+   const telegramLink2 = document.getElementById("telegram-link-2");
+  const telegramModal2 = document.getElementById("telegram-vpn-modal-2");
+  const telegramConfirm2 = document.getElementById("telegram-vpn-confirm-2");
+
+  telegramLink2.addEventListener("click", function (event) {
+    event.preventDefault(); // جلوگیری از باز شدن مستقیم
+    telegramModal2.classList.remove("hidden");
+  });
+
+  telegramConfirm2.addEventListener("click", function () {
+    telegramModal2.classList.add("hidden");
+    window.open(
+      "https://t.me/Eh3unamm",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  });
+    // Mobile menu toggle
+      const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+      const mobileMenu = document.getElementById("mobile-menu");
+
+      mobileMenuBtn.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+      });
+
+      // Close menu when a link is clicked
+      const menuLinks = mobileMenu.querySelectorAll("a");
+      menuLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+          mobileMenu.classList.add("hidden");
+        });
+      });
+      
